46b8c717dbf67e10420fc12786e4bc7e263dd6cf
Boluwatife George <georgeshoes31@gmail.com>
2023-11-11 17:16:37 +0100

    third commit

--- src/Component/MoviesCard.jsx
+++ src/Component/MoviesCard.jsx
@@ -1,15 +1,15 @@
+
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const MovieCard = ({ movie }) => {
   return (
     <div className="movie-card">
-    <img src={movie.posterURL} alt={movie.title} />
-    <div className="movie-info">
-      <h3>{movie.title}</h3>
-      <p>Rating: {movie.rating}</p>
-      <Link to={`/movies/${encodeURIComponent(movie.title)}`}>View Details</Link>
-    </div>
+      <img src={movie.posterURL} alt={movie.title} />
+      <div className="movie-details">
+        <h3>{movie.title}</h3>
+        <p>{movie.description}</p>
+        <p>Rating: {movie.rating}</p>
+      </div>
     </div>
   );
 };
